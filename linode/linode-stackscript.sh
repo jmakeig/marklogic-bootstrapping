2d15b50939089230587d474e8b7d2ce2f6caee3d
Justin Makeig <jmpublic@makeig.com>
2011-09-05 19:53:29 -0700

    Adding MarkLogic and Node.js

--- linode/linode-stackscript.sh
+++ linode/linode-stackscript.sh
@@ -73,5 +73,56 @@ echo "vm.nr_hugepages = 292" >> /etc/sysctl.conf
 rpm -Uv http://download.fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
 yum -y install git
 
+# Install MarkLogic
+echo "Installing MarkLogic 4.2-6.1"
+yum -yq install gdb
+# curl -O http://developer.marklogic.com/download/binaries/4.2/MarkLogic-4.2-6.1.x86_64.rpm
+rpm -iv http://developer.marklogic.com/download/binaries/4.2/MarkLogic-4.2-6.1.x86_64.rpm
+# /etc/init.d/MarkLogic start
+
+# Install node.js
+yum -yq install openssl-devel
+
+# Node.js
+# https://github.com/joyent/node/wiki/Installation
+cd $HOME
+git clone --depth 1 git://github.com/joyent/node.git # or git clone git://github.com/joyent/node.git if you want to checkout a stable tag
+cd node
+git checkout v0.4.11 # optional.  Note that master is unstable.
+export JOBS=2 # optional, sets number of parallel commands.
+mkdir ~/local
+./configure --prefix=$HOME/local/node
+make
+make install
+echo 'export PATH=$HOME/local/node/bin:$PATH' >> ~/.bashrc
+echo 'export NODE_PATH=$HOME/local/node:$HOME/local/node/lib/node_modules' >> ~/.bashrc
+source ~/.bashrc
+
+# NPM
+curl http://npmjs.org/install.sh | clean=yes sh
+npm config set tar=/usr/local/bin/tar
+
+# http-proxy
+npm install http-proxy
+
+# socket.io
+# https://github.com/LearnBoost/Socket.IO
+npm install socket.io
+
+# Haraka
+# https://github.com/baudehlo/Haraka
+npm install -g Haraka
+
+# Install nginx
+# yum -yq install nginx
+
+# Install HAProxy
+# Proxy reqirements:
+#   HTTP
+#   Websockets
+#   Streaming uploads
+#   Usable configuration
+#   SSL (most don't)
+yum -yq install haproxy
+
 echo "Done"
-touch DONE

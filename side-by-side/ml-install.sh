e0d92ae70f3ba347a27b4afb1954ced32c12c431
Justin Makeig <jmpublic@makeig.com>
2012-05-23 15:01:01 -0700

    Fixing some paths and adding a default for today’s nightly

--- side-by-side/ml-install.sh
+++ side-by-side/ml-install.sh
@@ -1,5 +1,6 @@
-#! /bin/bash -x
-# Copyright 2011, Justin Makeig <justin-public+githug@makeig.com>
+#! /bin/bash
+# -x
+# Copyright 2011, Justin Makeig <justin-public+github@makeig.com>
 # 
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -17,9 +18,19 @@ ML=~/Library/MarkLogic
 DT=~/Library/Application\ Support/MarkLogic/Data
 LOGIN=jmakeig
 
-VER="$1"-"$2"
-URL=http://root.marklogic.com/nightly/builds/macosx-64/osx-intel64-43-build.marklogic.com/HEAD/pkgs."$2"/MarkLogic-"$VER"-x86_64.dmg
+
+# If there is no date provided, use today
+if [ -z "$2" ]
+then
+    NIGHTLY=`date "+%Y%m%d"`
+else
+    NIGHTLY=$2    
+fi
+
+VER="$1"-"$NIGHTLY"
+URL=http://root.marklogic.com/nightly/builds/macosx/osx-intel64-51-build.marklogic.com/HEAD/pkgs."$NIGHTLY"/MarkLogic-"$VER"-x86_64.dmg
 # URL=http://developer.marklogic.com/download/binaries/4.2/MarkLogic-4.2-6-x86_64.dmg
+# URL=http://community.marklogic.com/download/binaries/5.0/MarkLogic-5.0-3-x86_64.dmg
 
 echo "Shutting down and unlinking a previous installation…"
 ~/bin/ml.sh -u

e6f0d6bbcb4972e277cbce8a7d738a5e98d2c97a
Justin Makeig <jmpublic@makeig.com>
2014-08-04 16:56:27 -0700

    Changed name and added delete

--- query-console.sh
+++ query-console.sh
@@ -14,6 +14,11 @@ USER=admin
 PASSWORD='********'
 
 curl -fsS "http://$HOST:8000/qconsole/endpoints/workspaces.xqy" --digest --user "$USER":"$PASSWORD" | jq --raw-output '.[].workspace[].id' | while read -r line; do
+  # Export
   printf "Exporting workspace $line from $HOST\n"
   curl  -fsS "http://$HOST:8000/qconsole/endpoints/workspaces.xqy?wsid=$line&format=export" --digest --user "$USER":"$PASSWORD" > "$HOST"_"$line"_$(date +"%Y-%m-%dT%H:%M:%SZ").workspace
+  
+  # Delete
+  #printf "Deleting workspace $line from $HOST\n"
+  #curl -fsS "http://$HOST:8000/qconsole/endpoints/workspaces.xqy?wsid=$line" -X DELETE --digest --user "$USER":"$PASSWORD" 1>/dev/null
 done
